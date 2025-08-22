5750b694878da1840091d121b8a44a0a71c66cb3
727723euci024-bit <727723euci024@skcet.ac.in>
2025-08-22 15:22:55 +0530

    Remove Product-related files to fix JPA conflicts

--- springapp/src/main/java/com/example/springapp/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springapp.repository;
-
-import com.example.springapp.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
